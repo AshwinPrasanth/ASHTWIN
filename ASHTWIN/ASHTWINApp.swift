a29291c1e301e82ca5da196521bed5c4840f83d3
AshwinPrasanth <118043272+AshwinPrasanth@users.noreply.github.com>
2023-05-26 15:45:31 +0530

    Initial Commit

---
+++ ASHTWIN/ASHTWINApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASHTWINApp.swift
+//  ASHTWIN
+//
+//  Created by Ashwin on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ASHTWINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
